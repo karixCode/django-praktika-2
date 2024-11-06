c0a33b01663b508095900d1ffc60cbd108f2a32d
karix <eivanov200606@mail.ru>
2024-11-06 20:00:59 +0700

    login, logout, register templates

--- interior/views.py
+++ interior/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.template.context_processors import request
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def logout_view(request):
+    return render(request, 'logout.html')
+
+
